10816fcd3a80f8f51ffbcad9194837a156e9316e
brown7jazz <35845258+brown7jazz@users.noreply.github.com>
2025-11-07 16:15:26 -0300

    Changed utils

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
 // src/utils/api.js
-//export const API_URL = 'https://mini-ecommerce-backend-zu5q.onrender.com/api'; // <- PRODUCCIÓN
-export const API_URL = 'http://localhost:5000/api';  // ← LOCAL
+export const API_URL = 'https://mini-ecommerce-backend-zu5q.onrender.com/api'; // <- PRODUCCIÓN
+//export const API_URL = 'http://localhost:5000/api';  // ← LOCAL
 
